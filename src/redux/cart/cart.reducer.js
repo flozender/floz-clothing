de3f96883e25ca78fdc6007cdac35041bab92b1a
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2020-04-15 16:51:25 +0530

    Added spinner HOC

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -3,7 +3,7 @@ import { addItemToCart, removeItemFromCart } from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
-  cartItems: [],
+  cartItems: null,
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
